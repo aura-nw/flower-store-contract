f33f6c66e293300790052a4239a5a29ea7b8d368
ThienLK <koankem0901@gmail.com>
2022-02-15 17:32:08 +0700

    init save

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,12 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-// use cosmwasm_std::Addr;
-use cw_storage_plus::{Map};
+use cosmwasm_storage::{bucket, bucket_read, Bucket, ReadonlyBucket};
+
+use cosmwasm_std::Storage;
+// use cw_storage_plus::Map;
+
+static STORE_KEY: &[u8] = b"store";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Flower {
@@ -12,4 +16,12 @@ pub struct Flower {
     pub price: i32,
 }
 
-pub const FLOWER: Map<'a, (&'a id, &'a id), Expiration> = Map::new("flower");
+// pub const FLOWER: Map<'a, (&'a String, &'a id), Expiration> = Map::new("flower");
+
+pub fn store(storage: &mut dyn Storage) -> Bucket<Flower> {
+    bucket(storage, STORE_KEY)
+}
+
+pub fn store_query(storage: &dyn Storage) -> ReadonlyBucket<Flower> {
+    bucket_read(storage, STORE_KEY)
+}
