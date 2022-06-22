cf3ba955cc67e661e8ee22c0f2de04d484f6aae2
nttnguyen136 <nttnguyen136@gmail.com>
2022-06-22 08:15:51 +0700

    update msg

--- src/contract.rs
+++ src/contract.rs
@@ -101,7 +101,7 @@ pub fn sell(deps: DepsMut, id: String, amount: i32) -> Result<Response, Contract
         }
     })?;
 
-    Ok(Response::new().add_attribute("action", "sell"))
+    Ok(Response::new())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
